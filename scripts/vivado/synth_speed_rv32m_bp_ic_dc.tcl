abfa54a9c68a127bc271e1c014a7d2ea74badf9d
Angel Terrones <angelterrones@gmail.com>
2019-11-05 17:11:19 -0400

    Update vivado scripts

--- scripts/vivado/synth_speed_rv32m_bp_ic_dc.tcl
+++ scripts/vivado/synth_speed_rv32m_bp_ic_dc.tcl
@@ -1,9 +1,9 @@
 read_verilog scripts/vivado/top.v
-read_verilog build/bellatrix_ic_w2/bellatrix_core.v
+read_verilog build/bellatrix_rv32m_bp_ic_dc/bellatrix_core.v
 read_xdc scripts/vivado/synth_speed.xdc
 
 synth_design -part xc7a100tftg256-2 -top top
-opt_design -directive Explore
+opt_design
 
 report_utilization
 report_timing
