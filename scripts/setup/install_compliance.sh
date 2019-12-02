8f81a8e815bf9a6fb15156f074d48725dd58559e
Angel Terrones <angelterrones@gmail.com>
2019-12-01 22:22:07 -0400

    scripts.setup: improve install messages.

--- scripts/setup/install_compliance.sh
+++ scripts/setup/install_compliance.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+Color_Off='\033[0m'
+BGreen='\033[1;32m'
+BYellow='\033[1;33m'
+
 RVCOMPLIANCE=$ROOT/tests/riscv-compliance
 
 if [ ! -d "$RVCOMPLIANCE" ]; then
-    echo "Downloading riscv-compliance"
+    echo -e ${BYellow}"Downloading riscv-compliance"${Color_Off}
     git clone https://github.com/AngelTerrones/riscv-compliance $RVCOMPLIANCE
     cd $RVCOMPLIANCE
     git checkout nht-cores
-    echo "Done!"
+    echo -e ${BGreen}"Done!"${Color_Off}
 else
     cd $RVCOMPLIANCE
+    echo -e ${BYellow}"Moving to branch 'nht-cores'"${Color_Off}
     git checkout nht-cores
+    echo -e ${BYellow}"Updating repository"${Color_Off}
     git pull
-    echo "Done!"
+    echo -e ${BGreen}"Done!"${Color_Off}
 fi
