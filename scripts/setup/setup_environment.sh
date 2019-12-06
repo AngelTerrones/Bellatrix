f3f92d81f11a06f3964d558d5ebf015904e475f0
Angel Terrones <angelterrones@gmail.com>
2019-12-06 12:03:37 -0400

    Update install script

--- scripts/setup/setup_environment.sh
+++ scripts/setup/setup_environment.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+Color_Off='\033[0m'
+BGreen='\033[1;32m'
+BYellow='\033[1;33m'
+BRed='\033[1;31m'
+
 VENV=$ROOT/.venv
 
 if [ -d "$VENV" ]; then
-    echo "Deleting old virtualenv"
+    echo -e ${BRed}"Deleting old virtualenv"${Color_Off}
     rm -rf $VENV
 fi
 
-echo "Creating python virtual environment"
+echo -e ${BYellow}"Creating python virtual environment"${Color_Off}
 python3 -m venv $VENV
-echo "Installing nMigen"
 source $VENV/bin/activate
+echo -e ${BYellow}"Installing required packages for development"${Color_Off}
 pip3 install flake8 rope wheel
+echo -e ${BYellow}"Installing nMigen"${Color_Off}
 pip3 install git+https://github.com/m-labs/nmigen.git
 deactivate
-echo "Virtualenv setup: DONE"
+echo -e ${BGreen}"Virtualenv setup: DONE"${Color_Off}
