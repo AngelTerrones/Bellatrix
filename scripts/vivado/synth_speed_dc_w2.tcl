c2b920f5c82a54511dddccfe456e2960af36c4ac
Angel Terrones <angelterrones@gmail.com>
2020-01-02 11:40:11 -0400

    scripts: Remove scrips for vivado.

--- scripts/vivado/synth_speed_dc_w2.tcl
+++
@@ -1,9 +0,0 @@
-read_verilog scripts/vivado/top.v
-read_verilog build/bellatrix_dc_w2/bellatrix_core.v
-read_xdc scripts/vivado/synth_speed.xdc
-
-synth_design -part xc7a100tftg256-2 -top top
-opt_design -directive Explore
-
-report_utilization
-report_timing
