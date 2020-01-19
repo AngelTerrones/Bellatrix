b3b60b9d9741823eeef2a5851641e7252065dcd5
Angel Terrones <angelterrones@gmail.com>
2020-01-19 18:56:11 -0400

    WIP: add color

--- cli.py
+++ cli.py
@@ -11,33 +11,38 @@ logo = r'''--------------------------------------------------
      ___      _ _      _       _
     | _ ) ___| | |__ _| |_ _ _(_)_ __
     | _ \/ -_) | / _` |  _| '_| \ \ /
-    |___/\___|_|_\__,_|\__|_| |_/_\_\
+    |___/\___|_|_\__,_|\__|_| |_/_\_\\
 
     A 32-bit RISC-V CPU based on nMigen
 --------------------------------------------------'''
 current_path = os.path.dirname(os.path.realpath(__file__))
-cpu_variants = ['minimal', 'lite', 'standard', 'full']
+cpu_variants = ['minimal', 'lite', 'standard', 'full', 'custom']
 
 
-def load_config(variant: str) -> Dict:
+def load_config(variant: str, configfile: str) -> Dict:
     # default path to configurations
-    configfile  = '{}/configurations/bellatrix_{}.yml'.format(current_path, variant)
+    if variant != 'custom':
+        configfile  = '{}/configurations/bellatrix_{}.yml'.format(current_path, variant)
+    elif configfile == '':
+        raise RuntimeError('Configuration file empty for custom variant.')
+
     core_config = yaml.load(open(configfile).read(), Loader=yaml.Loader)
 
     config = {}
 
     # print configuration
-    print(r'''{}
-=== Configuration name ===
+    print('''\033[1;33m{}\033[0m
 
-{}
+\033[0;32mConfiguration\033[0;0m
+Variant name: {}
+Path config file: {}
 
-=== Build parameters ==='''.format(logo, variant))
+\033[0;32mBuild parameters\033[0;0m'''.format(logo, variant, configfile))
 
     # translate and print parameters
     for key, item in core_config.items():
         if isinstance(item, dict):
-            print(f'\n{key}:')
+            print(f'{key}:')
             for k2, i2 in item.items():
                 config['{}_{}'.format(key, k2)] = i2
                 if isinstance(i2, int) and not isinstance(i2, bool):
@@ -54,7 +59,7 @@ def load_config(variant: str) -> Dict:
 
 def generate_verilog(parser, args):
     # load configuration
-    core_config = load_config(args.variant)
+    core_config = load_config(args.variant, os.path.realpath(args.config_file))
 
     # create the core
     cpu = Bellatrix(**core_config)
@@ -113,7 +118,7 @@ def main() -> None:
 
     parser = argparse.ArgumentParser(
         formatter_class=custom_formatter,
-        description='''{}'''.format(logo)
+        description='''\033[1;33m{}\033[0m'''.format(logo)
     )
 
     # --------------------------------------------------------------------------
@@ -123,6 +128,11 @@ def main() -> None:
         choices=cpu_variants,
         default='minimal',
         help='cpu variant'
+    )
+    parser.add_argument(
+        '--config-file',
+        default='',
+        help='configuration file for custom variants'
     )
     # --------------------------------------------------------------------------
     cli.main_parser(parser)
